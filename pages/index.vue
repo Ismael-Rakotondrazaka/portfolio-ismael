742d1a09b55634c520b13844d2e10d5cba0428fa
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-01-30 17:17:47 +0300

    Chore: Remove unnecessary content in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -9,30 +9,6 @@
     <TheProjects />
 
     <TheEducation />
-
-    <h1 class="text-red-600 text-xl bg-red-600/10 text-center">Ismael</h1>
-
-    <div>
-      <form>
-        <select v-model="locale">
-          <option value="en">en</option>
-          <option value="fr">fr</option>
-        </select>
-        <p>{{ $t("welcome") }}</p>
-      </form>
-    </div>
-
-    <ButtonUI> Lorem, ipsum dolor. </ButtonUI>
-
-    <ButtonUI variant="outlined"> Lorem, ipsum dolor. </ButtonUI>
-
-    <p class="text-primary bg-secondary">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa vero
-      repellendus expedita temporibus quisquam consequuntur odit pariatur unde
-      aspernatur ratione!
-    </p>
-
-    <h2 class="text-primary text-xl font-bold">Lorem, ipsum dolor.</h2>
   </main>
 </template>
 
