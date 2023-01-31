06cb61250acc7806e10c59d9475a81951f0df5d2
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-01-31 17:13:02 +0300

    Feat: Make TheWelcome component responsive

--- components/TheWelcome.vue
+++ components/TheWelcome.vue
@@ -1,37 +1,46 @@
 <template>
-  <div class="relative min-h-screen flex flex-row items-center justify-evenly">
-    <div class="absolute top-1/4 left-[2%] w-20">
+  <div
+    class="relative py-20 min-h-screen flex flex-row flex-wrap lg:flex-nowrap items-center justify-evenly"
+  >
+    <div class="absolute top-16 lg:top-1/5 left-[2%] w-7 md:w-12 lg:w-20">
       <FaIcon
         icon="fa-solid fa-code"
         class="text-primary w-full !h-auto hover:animate-spin"
       />
     </div>
 
-    <div class="absolute top-[40%] right-[4%] w-20 rotate-12">
+    <div class="absolute top-[40%] right-[4%] w-7 md:w-12 lg:w-20 rotate-12">
       <FaIcon
         icon="fa-solid fa-globe"
         class="text-primary w-full !h-auto hover:animate-bounce"
       />
     </div>
 
-    <div class="absolute bottom-[10%] right-[60%] w-20 -rotate-12">
+    <div
+      class="absolute bottom-[10%] right-[60%] w-7 md:w-12 lg:w-20 -rotate-12"
+    >
       <FaIcon
         icon="fa-solid fa-terminal"
         class="text-primary w-full !h-auto hover:animate-ping"
       />
     </div>
 
-    <div>
+    <div class="p-3 mb-10 md:mb-0 w-full lg:w-auto">
       <p class="mb-5">
-        <span class="font-bold text-2xl text-gray-500">Hi, I am</span>
+        <span class="font-bold text-lg md:text-2xl text-gray-500">
+          Hi, I am
+        </span>
+
         <br />
+
         <span
-          class="font-bold text-4xl text-primary after:w-0 after:h-[0.15rem] after:transition-all after:bg-primary after:block hover:after:w-full"
-          >Fitia Ismael Rakotondrazaka</span
+          class="font-bold text-2xl md:text-4xl text-primary after:w-0 after:h-[0.15rem] after:transition-all after:bg-primary after:block hover:after:w-full"
         >
+          Fitia Ismael Rakotondrazaka
+        </span>
       </p>
 
-      <h1 class="font-bold text-[4rem] bg-move leading-[1]">
+      <h1 class="font-bold text-5xl md:text-[4rem] bg-move leading-[1]">
         A professional
         <br />
         web developer
@@ -45,7 +54,7 @@
       </NuxtLink>
     </div>
 
-    <div class="w-full h-full max-w-[26rem] max-h-[26rem]">
+    <div class="p-3 w-full h-full max-w-[26rem] max-h-[26rem]">
       <img
         src="~/assets/images/profiles/profile.png"
         alt=""
