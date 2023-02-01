7e32021c988767fdfe6404bfbbe56366c6e877b3
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-02-01 20:54:17 +0300

    Feat: Make responsive TheHeader component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,11 +1,13 @@
 <template>
-  <header class="bg-secondary border-b-4 border-primary">
-    <nav class="w-full h-full flex flex-row items-center justify-center gap-10">
+  <header class="border-b-4 bg-secondary border-primary">
+    <nav
+      class="flex flex-row items-center justify-center w-full h-full gap-3 md:gap-7 lg:gap-10"
+    >
       <NuxtLink
         v-for="(link, index) in links"
         :key="index"
         :to="link.to"
-        class="text-white text-lg py-3 after:block after:bg-primary after:h-[0.15rem] after:w-0 hover:text-primary hover:after:w-full transition-all after:transition-all"
+        class="text-white lg:text-lg py-3 after:block after:bg-primary after:h-[0.15rem] after:w-0 hover:text-primary hover:after:w-full transition-all after:transition-all"
       >
         {{ link.text }}
       </NuxtLink>
